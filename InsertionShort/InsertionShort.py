0a1a5948c2aea6691c70eb101bd4ec02196a6800
panos1998 <petridisp@ece.auth.gr>
2021-09-20 12:31:56 +0300

    Folder order
    Folder order

--- InsertionShort/InsertionShort.py
+++ InsertionShort/InsertionShort.py
@@ -1,4 +1,4 @@
-b = [5,2,4,6,1,3,5,3,5,7,9,2,2]
+b = [5,2,4,6,8,3,5,3,5,7,9,2,2]
 def my_function(a):
     for j in range(1, len(a)): #iterate through second to last elements
         key = a[j] #select each time an increasing pivot element
