0ac9e62ec17e0c85a346ef0306f6ba852578a527
panos1998 <petridisp@ece.auth.gr>
2021-10-05 13:56:33 +0300

    comments

--- BarCut/dynamicrod.py
+++ BarCut/dynamicrod.py
@@ -6,10 +6,10 @@ value = [1, 5, 8, 9, 10, 17, 17, 20, 24, 30]
 def dynamicrod(p, n):
     r = [0 for k in range(n+1)]
     r[0] = 0
-    for j in range(1, n+1):
+    for j in range(1, n+1): # for each bar element
         q = -math.inf
-        for i in range(j):
-            q = max(q, p[i] + r[j-1-i])
+        for i in range(j): # for each bar element from start to  selected element p[j]
+            q = max(q, p[i] + r[j-1-i]) # solve the subproblems
         r[j] = q
     return r[n]
 
