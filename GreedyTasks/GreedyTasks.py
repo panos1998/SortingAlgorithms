59199f3ebb54e73df3d6a6c8769c09ea2b1c7694
panos1998 <petridisp@ece.auth.gr>
2021-10-14 15:16:13 +0300

    built-in f array sorting

--- GreedyTasks/GreedyTasks.py
+++ GreedyTasks/GreedyTasks.py
@@ -2,7 +2,7 @@ start = [1, 3, 0, 5, 3, 5, 6, 8, 8, 2, 12]
 finish = [4, 5, 6, 7, 8, 9, 10, 11, 12, 14, 16]
 
 def greedytasks(s, f):
-    # f array in asceding order
+    f.sort  # finish sorted asceding
     n = len(s)
     iterator = 0
     a = ["a{}".format(iterator)]  # by default add first task  to solution
